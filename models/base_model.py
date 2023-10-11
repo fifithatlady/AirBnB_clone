9d5bfb1b1cad7b8aca1f4e4c4ab4de296922f3a3
CarltonNcube <carltonncube@outlook.com>
2023-10-11 08:44:54 -0700

    a class BaseModel that defines all common attributes/methods for other classes

---
+++ models/base_model.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Defines the BaseModel class."""
+
+import uuid
+from datetime import datetime
+import models
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+
+        for key, value in kwargs.items():
+            if key == "created_at" or key == "updated_at":
+                setattr(self, key, datetime.strptime(value, tform))
+            else:
+                setattr(self, key, value)
+
+        if not kwargs:
+            models.storage.new(self)
+
+    def save(self):
+        """Update updated_at with the current datetime and save to storage."""
+        self.updated_at = datetime.today()
+        models.storage.save()
+
+    def to_dict(self):
+        """Return a dictionary representation of the BaseModel instance."""
+        result = {**self.__dict__}
+        result["created_at"] = self.created_at.isoformat()
+        result["updated_at"] = self.updated_at.isoformat()
+        result["__class__"] = self.__class__.__name
+        return result
+
+    def __str__(self):
+        """Return a string representation of the BaseModel instance."""
+        class_name = self.__class__.__name
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
